ebc689725907dd6555022b3a07059d193a0aa026
Lucas Sampaio <lucas.sampaio@grupozap.com>
2020-06-09 22:00:32 -0300

    exercise level 2

---
+++ src/level_2/ex4.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	fmt.Printf("%d\n", a)
+	fmt.Printf("%b\n", a)
+	fmt.Printf("%#x\n", a)
+	b := a << 1
+	fmt.Printf("%d\n", b)
+	fmt.Printf("%b\n", b)
+	fmt.Printf("%#x\n", b)
+}
